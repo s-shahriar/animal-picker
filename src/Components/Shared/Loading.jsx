329a8df2e2323fdafa61f9e35671537b1361a2e3
s-shahriar <ksnkkc@gmail.com>
2024-09-20 00:01:06 +0600

    broken css fix

--- src/Components/Shared/Loading.jsx
+++ src/Components/Shared/Loading.jsx
@@ -1,7 +1,7 @@
 const Loading = () => {
   return (
     <div className="flex justify-center items-center h-64">
-      <p className="text-lg">Query Loading......</p>
+      <p className="text-lg">Query Loading.......</p>
     </div>
   );
 };
